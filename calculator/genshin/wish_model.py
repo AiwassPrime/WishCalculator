00b68bf348a3cab4e6c6c12346946eb7e43ac0fe
AiwassPrime <flnww@AiwassPrime>
2023-09-30 11:24:28 +0800

    add weapon

--- calculator/genshin/wish_model.py
+++ calculator/genshin/wish_model.py
@@ -27,18 +27,92 @@ class GenshinWishModel:
 
     def get_next_states(self):
         if self.plan[0] == 0:
-            get = self.chara[self.state[0][0]]
-            no_get = 1 - get
-            if self.state[0][0] == 0:
-                get_half = 0.5 * get
+            get_chance = self.chara[self.state[0][0]]
+            if get_chance > 1:
+                get_chance = 1
+            no_get_chance = 1 - get_chance
+            if self.state[0][1] == 0:
+                get_chance_half = 0.5 * get_chance
                 get_chara = GenshinWishModel(self.chara, self.weapon, state=((0, 0), self.state[1]), plan=self.plan[1:])
                 get_pity = GenshinWishModel(self.chara, self.weapon, state=((0, 1), self.state[1]), plan=self.plan)
-                get_nothing = GenshinWishModel(self.chara, self.weapon, state=((self.state[0][0]+1, 0), self.state[1]), plan=self.plan)
-                return [(get_half, get_chara), (get_half, get_pity), (no_get, get_nothing)]
-            else:
+                get_nothing = GenshinWishModel(self.chara, self.weapon,
+                                               state=((self.state[0][0]+1, 0), self.state[1]), plan=self.plan)
+                if get_chance >= 1:
+                    return [(get_chance_half, get_chara), (get_chance_half, get_pity)]
+                else:
+                    return [(get_chance_half, get_chara), (get_chance_half, get_pity), (no_get_chance, get_nothing)]
+            elif self.state[0][1] == 1:
                 get_chara = GenshinWishModel(self.chara, self.weapon, state=((0, 0), self.state[1]), plan=self.plan[1:])
-                get_nothing = GenshinWishModel(self.chara, self.weapon, state=((self.state[0][0]+1, 1), self.state[1]), plan=self.plan)
-                return [(get, get_chara), (no_get, get_nothing)]
+                get_nothing = GenshinWishModel(self.chara, self.weapon,
+                                               state=((self.state[0][0]+1, 1), self.state[1]), plan=self.plan)
+                if get_chance >= 1:
+                    return [(get_chance, get_chara)]
+                else:
+                    return [(get_chance, get_chara), (no_get_chance, get_nothing)]
+            else:
+                raise Exception("Unexpected state: " + str(self.state[0]))
+        elif self.plan[0] == 0:
+            get_chance = self.weapon[self.state[1][0]]
+            if get_chance > 1:
+                get_chance = 1
+            no_get_chance = 1 - get_chance
+            if self.state[1][1] == 0 and self.state[1][2] < 2:
+                get_chance_reduction = 0.375 * get_chance
+                get_weapon = GenshinWishModel(self.chara, self.weapon,
+                                              state=(self.state[0], (0, 0, 0)), plan=self.plan[1:])
+                get_pity = GenshinWishModel(self.chara, self.weapon,
+                                            state=(self.state[0], (0, 1, self.state[1][2] + 1)), plan=self.plan)
+                get_nothing = GenshinWishModel(self.chara, self.weapon,
+                                               state=(self.state[0], (self.state[1][0] + 1, 0, self.state[1][2])),
+                                               plan=self.plan)
+                if get_chance >= 1:
+                    return [(get_chance_reduction, get_weapon), (get_chance_reduction, get_pity)]
+                else:
+                    return [(get_chance_reduction, get_weapon), (get_chance_reduction, get_pity),
+                            (no_get_chance, get_nothing)]
+            elif self.state[1][1] == 1 and self.state[1][2] < 2:
+                get_chance_reduction = 0.75 * get_chance
+                get_weapon = GenshinWishModel(self.chara, self.weapon,
+                                              state=(self.state[0], (0, 0, 0)), plan=self.plan[1:])
+                get_nothing = GenshinWishModel(self.chara, self.weapon,
+                                               state=(self.state[0],
+                                                      (self.state[1][0] + 1, 1, self.state[1][2])),
+                                               plan=self.plan)
+                if get_chance >= 1:
+                    return [(get_chance_reduction, get_weapon)]
+                else:
+                    return [(get_chance_reduction, get_weapon), (no_get_chance, get_nothing)]
+            elif self.state[1][2] >= 2:
+                get_chance_reduction = 0.35 * get_chance
+                get_weapon = GenshinWishModel(self.chara, self.weapon,
+                                              state=(self.state[0], (0, 0, 0)), plan=self.plan[1:])
+                get_nothing = GenshinWishModel(self.chara, self.weapon,
+                                               state=(self.state[0],
+                                                      (self.state[1][0] + 1, self.state[1][1], self.state[1][2])),
+                                               plan=self.plan)
+                if get_chance >= 1:
+                    return [(get_chance_reduction, get_weapon)]
+                else:
+                    return [(get_chance_reduction, get_weapon), (no_get_chance, get_nothing)]
+            else:
+                raise Exception("Unexpected state: " + str(self.state[1]))
         else:
-            get = 0
+            raise Exception("Unexpected plan: " + str(self.plan))
 
+
+if __name__ == "__main__":
+    charaList = []
+    for i in range(100):
+        if i <= 73:
+            charaList.append(60 / 10000)
+        else:
+            charaList.append((60 + (600 * (i - 73))) / 10000)
+    weaponList = []
+    for i in range(100):
+        if i <= 62:
+            weaponList.append(70 / 10000)
+        else:
+            weaponList.append((70 + (700 * (i - 62))) / 10000)
+    model = GenshinWishModel(charaList, weaponList, plan=[0])
+    print(model.get_state())
+    print(model.get_next_states())
