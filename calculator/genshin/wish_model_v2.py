9b3a85de34cb418a766d39021743f6e593b524a8
AiwassPrime <flnww@AiwassPrime>
2023-10-04 22:01:23 +0800

    finish model v2

--- calculator/genshin/wish_model_v2.py
+++ calculator/genshin/wish_model_v2.py
@@ -1,7 +1,14 @@
 import pickle
 import os
+import time
+from typing import Dict
 
 model_file_path = '../models/genshin_v2.pkl'
+
+
+class GenshinWishModelState(tuple[tuple[int, int], tuple[int, int, int], list[int]]):
+    def __str__(self):
+        return str((self[0], self[1], ''.join(map(str, self[2]))))
 
 
 def is_cache_exist():
@@ -16,15 +23,17 @@ class GenshinWishModelV2:
     chara_cache = {}
     weapon_cache = {}
 
-    def __init__(self):
-        if len(self.chara) != 0:
+    def __init__(self, force=False):
+        if not force and len(self.chara) != 0:
             return
-        if is_cache_exist():
+        if not force and is_cache_exist():
             self.__load_cache()
         else:
+            start = time.time()
             self.__fill_chance()
             self.__fill_cache()
             self.__dump_cache()
+            print("Build model in " + str(time.time() - start) + " second(s)")
 
     def __dump_cache(self):
         cache = {
@@ -63,90 +72,127 @@ class GenshinWishModelV2:
     def __fill_cache(self):
         chara_cache = {}
         bfs_queue = [(0, 0)]
+        bfs_set = set()
         while len(bfs_queue) > 0:
             curr_process = bfs_queue.pop()
             if self.chara[curr_process[0]] >= 1:
                 if curr_process[1] >= 1:
-                    chara_cache[curr_process] = (1.0, (0, 0), True)  # get want
+                    chara_cache.setdefault(curr_process, []).append((1.0, (0, 0), True))  # get want
                 else:
-                    chara_cache[curr_process] = (0.5, (0, 0), True)  # get want
-                    chara_cache[curr_process] = (0.5, (0, 1), False)  # get pity
-                    if (0, 1) not in chara_cache:
+                    chara_cache.setdefault(curr_process, []).append((0.5, (0, 0), True))  # get want
+                    chara_cache.setdefault(curr_process, []).append((0.5, (0, 1), False))  # get pity
+                    if (0, 1) not in bfs_set:
+                        bfs_set.add((0, 1))
                         bfs_queue.append((0, 1))
             else:
                 if curr_process[1] >= 1:
-                    chara_cache[curr_process] = (self.chara[curr_process[0]], (0, 0), True)  # get want
-                    chara_cache[curr_process] = (
-                        1.0 - self.chara[curr_process[0]], (curr_process[0] + 1, 1), False)  # get nothing
-                    if (curr_process[0] + 1, 1) not in chara_cache:
+                    chara_cache.setdefault(curr_process, []).append(
+                        (self.chara[curr_process[0]], (0, 0), True))  # get want
+                    chara_cache.setdefault(curr_process, []).append((
+                        1.0 - self.chara[curr_process[0]], (curr_process[0] + 1, 1), False))  # get nothing
+                    if (curr_process[0] + 1, 1) not in bfs_set:
+                        bfs_set.add((curr_process[0] + 1, 1))
                         bfs_queue.append((curr_process[0] + 1, 1))
                 else:
-                    chara_cache[curr_process] = (self.chara[curr_process[0]] * 0.5, (0, 0), True)  # get want
-                    chara_cache[curr_process] = (self.chara[curr_process[0]] * 0.5, (0, 1), False)  # get pity
-                    chara_cache[curr_process] = (
-                        1.0 - self.chara[curr_process[0]], (curr_process[0] + 1, 0), False)  # get nothing
-                    if (0, 1) not in chara_cache:
+                    chara_cache.setdefault(curr_process, []).append(
+                        (self.chara[curr_process[0]] * 0.5, (0, 0), True))  # get want
+                    chara_cache.setdefault(curr_process, []).append(
+                        (self.chara[curr_process[0]] * 0.5, (0, 1), False))  # get pity
+                    chara_cache.setdefault(curr_process, []).append((
+                        1.0 - self.chara[curr_process[0]], (curr_process[0] + 1, 0), False))  # get nothing
+                    if (0, 1) not in bfs_set:
+                        bfs_set.add((0, 1))
                         bfs_queue.append((0, 1))
-                    if (curr_process[0] + 1, 0) not in chara_cache:
+                    if (curr_process[0] + 1, 0) not in bfs_set:
+                        bfs_set.add((curr_process[0] + 1, 0))
                         bfs_queue.append((curr_process[0] + 1, 0))
+        self.chara_cache = chara_cache
+
         weapon_cache = {}
-        bfs_queue = [(0, 0, 0)]
+        bfs_queue = [(0, 0, 0), (0, 1, 0)]
+        bfs_set = set()
         while len(bfs_queue) > 0:
             curr_process = bfs_queue.pop()
-            if self.chara[curr_process[0]] >= 1:
+            if self.weapon[curr_process[0]] >= 1:
                 if curr_process[2] >= 2:
-                    weapon_cache[curr_process] = (1.0, (0, 0, 0), True)  # get want
+                    weapon_cache.setdefault(curr_process, []).append((1.0, (0, 0, 0), True))  # get want
                 elif curr_process[1] >= 1:
-                    weapon_cache[curr_process] = (0.5, (0, 0, 0), True)  # get want
-                    weapon_cache[curr_process] = (0.5, (0, 0, curr_process[2] + 1), False)  # get other
-                    if (0, 0, curr_process[2] + 1) not in weapon_cache:
+                    weapon_cache.setdefault(curr_process, []).append((0.5, (0, 0, 0), True))  # get want
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (0.5, (0, 0, curr_process[2] + 1), False))  # get other
+                    if (0, 0, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 0, curr_process[2] + 1))
                         bfs_queue.append((0, 0, curr_process[2] + 1))
                 else:
-                    weapon_cache[curr_process] = (0.375, (0, 0, 0), True)  # get want
-                    weapon_cache[curr_process] = (0.375, (0, 0, curr_process[2] + 1), False)  # get other
-                    weapon_cache[curr_process] = (0.25, (0, 1, curr_process[2] + 1), False)  # get pity
-                    if (0, 0, curr_process[2] + 1) not in weapon_cache:
+                    weapon_cache.setdefault(curr_process, []).append((0.375, (0, 0, 0), True))  # get want
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (0.375, (0, 0, curr_process[2] + 1), False))  # get other
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (0.25, (0, 1, curr_process[2] + 1), False))  # get pity
+                    if (0, 0, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 0, curr_process[2] + 1))
                         bfs_queue.append((0, 0, curr_process[2] + 1))
-                    if (0, 1, curr_process[2] + 1) not in weapon_cache:
+                    if (0, 1, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 1, curr_process[2] + 1))
                         bfs_queue.append((0, 1, curr_process[2] + 1))
             else:
                 if curr_process[2] >= 2:
-                    weapon_cache[curr_process] = (self.weapon[curr_process[0]], (0, 0, 0), True)  # get want
-                    weapon_cache[curr_process] = (
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (self.weapon[curr_process[0]], (0, 0, 0), True))  # get want
+                    weapon_cache.setdefault(curr_process, []).append((
                         1 - self.weapon[curr_process[0]], (curr_process[0] + 1, curr_process[1], 2),
-                        False)  # get nothing
-                    if (curr_process[0] + 1, curr_process[1], 2) not in weapon_cache:
+                        False))  # get nothing
+                    if (curr_process[0] + 1, curr_process[1], 2) not in bfs_set:
+                        bfs_set.add((curr_process[0] + 1, curr_process[1], 2))
                         bfs_queue.append((curr_process[0] + 1, curr_process[1], 2))
                 elif curr_process[1] >= 1:
-                    weapon_cache[curr_process] = (self.weapon[curr_process[0]] * 0.5, (0, 0, 0), True)  # get want
-                    weapon_cache[curr_process] = (
-                        self.weapon[curr_process[0]] * 0.5, (0, 0, curr_process[2] + 1), False)  # get other
-                    weapon_cache[curr_process] = (
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (self.weapon[curr_process[0]] * 0.5, (0, 0, 0), True))  # get want
+                    weapon_cache.setdefault(curr_process, []).append((
+                        self.weapon[curr_process[0]] * 0.5, (0, 0, curr_process[2] + 1), False))  # get other
+                    weapon_cache.setdefault(curr_process, []).append((
                         1.0 - self.weapon[curr_process[0]], (curr_process[0] + 1, 1, curr_process[2]),
-                        False)  # get nothing
-                    if (0, 0, curr_process[2] + 1) not in weapon_cache:
+                        False))  # get nothing
+                    if (0, 0, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 0, curr_process[2] + 1))
                         bfs_queue.append((0, 0, curr_process[2] + 1))
-                    if (curr_process[0] + 1, 1, curr_process[2]) not in weapon_cache:
+                    if (curr_process[0] + 1, 1, curr_process[2]) not in bfs_set:
+                        bfs_set.add((curr_process[0] + 1, 1, curr_process[2]))
                         bfs_queue.append((curr_process[0] + 1, 1, curr_process[2]))
                 else:
-                    weapon_cache[curr_process] = (self.weapon[curr_process[0]] * 0.375, (0, 0, 0), True)  # get want
-                    weapon_cache[curr_process] = (
-                        self.weapon[curr_process[0]] * 0.375, (0, 0, curr_process[2] + 1), False)  # get other
-                    weapon_cache[curr_process] = (
-                        self.weapon[curr_process[0]] * 0.25, (0, 1, curr_process[2] + 1), False)  # get pity
-                    weapon_cache[curr_process] = (
+                    weapon_cache.setdefault(curr_process, []).append(
+                        (self.weapon[curr_process[0]] * 0.375, (0, 0, 0), True))  # get want
+                    weapon_cache.setdefault(curr_process, []).append((
+                        self.weapon[curr_process[0]] * 0.375, (0, 0, curr_process[2] + 1), False))  # get other
+                    weapon_cache.setdefault(curr_process, []).append((
+                        self.weapon[curr_process[0]] * 0.25, (0, 1, curr_process[2] + 1), False))  # get pity
+                    weapon_cache.setdefault(curr_process, []).append((
                         1.0 - self.weapon[curr_process[0]], (curr_process[0] + 1, 0, curr_process[2]),
-                        False)  # get nothing
-                    if (0, 0, curr_process[2] + 1) not in weapon_cache:
+                        False))  # get nothing
+                    if (0, 0, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 0, curr_process[2] + 1))
                         bfs_queue.append((0, 0, curr_process[2] + 1))
-                    if (0, 1, curr_process[2] + 1) not in weapon_cache:
+                    if (0, 1, curr_process[2] + 1) not in bfs_set:
+                        bfs_set.add((0, 1, curr_process[2] + 1))
                         bfs_queue.append((0, 1, curr_process[2] + 1))
-                    if (curr_process[0] + 1, 0, curr_process[2]) not in weapon_cache:
+                    if (curr_process[0] + 1, 0, curr_process[2]) not in bfs_set:
+                        bfs_set.add((curr_process[0] + 1, 0, curr_process[2]))
                         bfs_queue.append((curr_process[0] + 1, 0, curr_process[2]))
+        self.weapon_cache = weapon_cache
 
-    def get_next_states(self):
-        pass
+    def get_next_states(self, curr_state: GenshinWishModelState) -> list[tuple[float, GenshinWishModelState, bool]]:
+        if len(curr_state[2]) <= 0:
+            return [(1.0, curr_state, True)]
+        if curr_state[2][0] == 0:
+            return [(chance, GenshinWishModelState(
+                (chara_state, curr_state[1], (curr_state[2][1:] if is_want else curr_state[2]))), False) for
+                    chance, chara_state, is_want in self.chara_cache[curr_state[0]]]
+        else:
+            return [(chance, GenshinWishModelState(
+                (curr_state[0], weapon_state, (curr_state[2][1:] if is_want else curr_state[2]))), False) for
+                    chance, weapon_state, is_want in self.weapon_cache[curr_state[1]]]
 
 
 if __name__ == "__main__":
-    pass
+    model = GenshinWishModelV2()
+    print(model.get_next_states(GenshinWishModelState(((74, 0), (1, 0, 0), [1]))))
